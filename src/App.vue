b977d478f3a29d8defd725a42aeb8b8fd0cc23b6
Tigran Ghumashyan <tigranghumashyan@gmail.com>
2024-05-10 16:48:51 +0400

    First commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import { ref } from 'vue'
+
+  let count = ref(0)
+
+  const increment = () => count.value++
+</script>
+
+<template>
+  <h2>Count: {{ count }}</h2>
+  <button @click="increment">+</button>
+  <input v-model="count"/>
+</template>
+
+<style scoped>
+
+</style>
