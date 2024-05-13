4d945495e23614c44c3d35d208f5bb60b2735a09
Tigran Ghumashyan <tigranghumashyan@gmail.com>
2024-05-13 17:52:07 +0400

    Changes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,61 @@
 <script setup>
-  import { ref } from 'vue'
+import { ref, provide, watch, computed } from 'vue'
 
-  let count = ref(0)
+import Header from './components/Header.vue'
+import Drawer from './components/Drawer.vue'
 
-  const increment = () => count.value++
+/* Корзина (START) */
+const cart = ref([])
+const drawerOpen = ref(false)
+
+const totalPrice = computed(() => cart.value.reduce((acc, item) => acc + item.price, 0))
+const vatPrice = computed(() => Math.round((totalPrice.value * 5) / 100))
+
+const closeDrawer = () => {
+  drawerOpen.value = false
+}
+
+const openDrawer = () => {
+  drawerOpen.value = true
+}
+
+const addToCart = (item) => {
+  cart.value.push(item)
+  item.isAdded = true
+}
+
+const removeFromCart = (item) => {
+  cart.value.splice(cart.value.indexOf(item), 1)
+  item.isAdded = false
+}
+
+watch(
+  cart,
+  () => {
+    localStorage.setItem('cart', JSON.stringify(cart.value))
+  },
+  { deep: true }
+)
+
+provide('cart', {
+  cart,
+  closeDrawer,
+  openDrawer,
+  addToCart,
+  removeFromCart
+})
+
+/* Корзина (END) */
 </script>
 
 <template>
-  <h2>Count: {{ count }}</h2>
-  <button @click="increment">+</button>
-  <input v-model="count"/>
+  <Drawer v-if="drawerOpen" :total-price="totalPrice" :vat-price="vatPrice" />
+
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
+    <Header :total-price="totalPrice" @open-drawer="openDrawer" />
+
+    <div class="p-10">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
